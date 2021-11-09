88990bc87fd4717094745ab247ac8887bdf92f2e
Christopher Collins <collins.christopher@gmail.com>
2021-11-09 11:04:55 -1000

    Adds initial ldap package
    Adds a first pass at ldap querying.
    
    Signed-off-by: Christopher Collins <collins.christopher@gmail.com>

--- pkg/ldap/ldap.go
+++ pkg/ldap/ldap.go
@@ -1,7 +1,54 @@
 package ldap
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/go-ldap/ldap"
+	"github.com/openshift/compliance-audit-router/pkg/config"
+)
+
 func LookupUser(username string) (string, string, error) {
-	// TODO: implement
-	// return user, manager, error
-	return "", "", nil
+	ldapAddr := config.GetLDAPAddr()
+
+	fmt.Println(ldapAddr)
+	fmt.Println("DEBUG 2")
+
+	conn, err := ldap.DialURL("ldap://" + ldapAddr)
+	defer conn.Close()
+	if err != nil {
+		return "", "", err
+	}
+
+	fmt.Println("DEBUG 3")
+
+	if config.AppConfig.LDAPConfig.Username != "" {
+		fmt.Println("DEBUG 4")
+		_, err = conn.SimpleBind(&ldap.SimpleBindRequest{
+			Username: config.AppConfig.LDAPConfig.Username,
+			Password: config.AppConfig.LDAPConfig.Password,
+		})
+		fmt.Println("DEBUG 4a")
+	} else {
+		fmt.Println("DEBUG 5")
+		conn.UnauthenticatedBind("")
+		fmt.Println("DEBUG 5a")
+	}
+	if err != nil {
+		return "", "", err
+	}
+
+	fmt.Println("DEBUG 6")
+	searchRequest := ldap.NewSearchRequest(config.AppConfig.LDAPConfig.SearchBase, ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false, "(uid="+username+")", config.AppConfig.LDAPConfig.Attributes, nil)
+
+	fmt.Println("DEBUG 7")
+	result, err := conn.Search(searchRequest)
+	if err != nil {
+		return "", "", err
+	}
+
+	fmt.Println("DEBUG 8")
+	fmt.Printf("%+v\n", &result)
+
+	return "", "", errors.New("Not implemented")
 }
