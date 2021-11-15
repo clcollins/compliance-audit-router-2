c2dea34267477936a54de01a4e14aa4912ef37c6
Christopher Collins <collins.christopher@gmail.com>
2021-11-15 13:04:30 -1000

    Incremental work on ldap connection
    Incremental work on ldap connections and parsing the resulting data.
    
    Signed-off-by: Christopher Collins <collins.christopher@gmail.com>

--- pkg/ldap/ldap.go
+++ pkg/ldap/ldap.go
@@ -19,52 +19,58 @@ package ldap
 import (
 	"errors"
 	"fmt"
+	"log"
 
 	"github.com/go-ldap/ldap"
 	"github.com/openshift/compliance-audit-router/pkg/config"
 )
 
 func LookupUser(username string) (string, string, error) {
+
 	ldapAddr := config.GetLDAPAddr()
 
-	fmt.Println(ldapAddr)
-	fmt.Println("DEBUG 2")
-
-	conn, err := ldap.DialURL("ldap://" + ldapAddr)
+	conn, err := ldap.DialURL(ldapAddr)
 	defer conn.Close()
 	if err != nil {
 		return "", "", err
 	}
 
-	fmt.Println("DEBUG 3")
-
 	if config.AppConfig.LDAPConfig.Username != "" {
-		fmt.Println("DEBUG 4")
 		_, err = conn.SimpleBind(&ldap.SimpleBindRequest{
 			Username: config.AppConfig.LDAPConfig.Username,
 			Password: config.AppConfig.LDAPConfig.Password,
 		})
 		fmt.Println("DEBUG 4a")
 	} else {
-		fmt.Println("DEBUG 5")
 		conn.UnauthenticatedBind("")
-		fmt.Println("DEBUG 5a")
+		fmt.Println("DEBUG 4b")
 	}
 	if err != nil {
 		return "", "", err
 	}
 
-	fmt.Println("DEBUG 6")
-	searchRequest := ldap.NewSearchRequest(config.AppConfig.LDAPConfig.SearchBase, ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false, "(uid="+username+")", config.AppConfig.LDAPConfig.Attributes, nil)
+	searchRequest := ldap.NewSearchRequest(config.AppConfig.LDAPConfig.SearchBase,
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		"(uid="+username+")", config.AppConfig.LDAPConfig.Attributes, nil)
 
-	fmt.Println("DEBUG 7")
 	result, err := conn.Search(searchRequest)
 	if err != nil {
 		return "", "", err
 	}
+	fmt.Println("DEBUG 6")
 
-	fmt.Println("DEBUG 8")
-	fmt.Printf("%+v\n", &result)
+	if len(result.Entries) == 0 {
+		return "", "", errors.New("User not found")
+	} else {
+		for _, entry := range result.Entries {
+			for _, attribute := range entry.Attributes {
+				if len(attribute.Values) > 1 {
+					log.Printf("multiple attributes found for user %s: %s - using the first (%s)",
+						username, attribute.Name, attribute.Values)
+				}
+			}
+		}
+	}
 
 	return "", "", errors.New("Not implemented")
 }
