a06420d3b01c30c36a696a2f97dc9516a150a765
Christopher Collins <collins.christopher@gmail.com>
2022-03-02 15:46:43 -1000

    removing temporary Jira short-circuit
    Signed-off-by: Christopher Collins <collins.christopher@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -23,28 +23,24 @@ import (
 	"os"
 
 	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 
 	"github.com/openshift/compliance-audit-router/pkg/config"
-
-	"github.com/openshift/compliance-audit-router/pkg/jira"
+	"github.com/openshift/compliance-audit-router/pkg/listeners"
 )
 
 var portString = ":" + fmt.Sprint(config.AppConfig.ListenPort)
 
 func main() {
-	// 	r := mux.NewRouter()
-	// 	r.Use(loggingMiddleware)
-	//
-	// 	for _, listener := range listeners.Listeners {
-	// 		listeners.CreateListener(listener.Path, listener.Methods, listener.HandlerFunc).AddRoute(r)
-	// 	}
-	//
-	// 	log.Printf("Listening on %s", portString)
-	// 	log.Fatal(http.ListenAndServe(portString, r))
+	r := mux.NewRouter()
+	r.Use(loggingMiddleware)
 
-	log.Printf("STARTING JIRA AUTH TEST")
-	jira.Run()
+	for _, listener := range listeners.Listeners {
+		listeners.CreateListener(listener.Path, listener.Methods, listener.HandlerFunc).AddRoute(r)
+	}
 
+	log.Printf("Listening on %s", portString)
+	log.Fatal(http.ListenAndServe(portString, r))
 }
 
 // Mux middleware to log requests in the Apache combined logging format
